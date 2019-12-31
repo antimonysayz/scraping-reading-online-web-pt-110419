eb638a7fc78a6c6471a3fa2cef2442a18b57519d
Anthony Mai <anthony.mai530@gmail.com>
2019-12-31 02:43:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,6 +3,6 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(open(html))
+doc = Nokogiri::HTML(html)
 
-doc.css(".headline-260IBN")
+doc.css(".headline-260IB")
